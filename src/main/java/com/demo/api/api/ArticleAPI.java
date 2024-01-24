8156a72e601990d81df68cd1c5fb427213cbf199
Boryana Baeva <b.baeva@labom2iformation.fr>
2024-01-24 14:54:39 +0100

    Article : GetById, Delete and PUT

--- src/main/java/com/demo/api/api/ArticleAPI.java
+++ src/main/java/com/demo/api/api/ArticleAPI.java
@@ -1,7 +1,7 @@
 package com.demo.api.api;
 
+import com.demo.api.business.ArticleManager;
 import com.demo.api.data.Article;
-import com.demo.api.data.DAO;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -10,12 +10,11 @@ import java.util.List;
 
 @Path("/articles")
 public class ArticleAPI {
-    private final DAO<Article> articleDAO = new DAO<>(Article.class);
-
+    private final ArticleManager articleManager = ArticleManager.getInstance();
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAll() {
-        List<Article> articles = articleDAO.findAll();
+        List<Article> articles = articleManager.getAll();
         return Response.ok().entity(articles).build();
     }
 
@@ -32,8 +31,62 @@ public class ArticleAPI {
                     .entity("Le prix de l'article doit obligatoirement être défini!").build();
         }
         else  {
-            articleDAO.save(article);
+            articleManager.add(article);
             return Response.status(Response.Status.CREATED).entity(article).build();
+        }
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getById(@PathParam("id") Integer id){
+        Article article = articleManager.getById(id);
+        if(article == null) {
+            return Response.status(Response.Status.NOT_FOUND).entity("404 : Article introuvable !").build();
+        }
+        else {
+            return Response.ok().entity(article).build();
+        }
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response delete(@PathParam("id") Integer id){
+        Article article = articleManager.getById(id);
+        if(article == null) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Article inexistant !").build();
+        }
+        else {
+            articleManager.delete(article);
+            return Response.ok().entity("Article supprimé avec succès !").build();
+        }
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response update(Article article, @PathParam("id") Integer id){
+        if(article == null){
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Article non existant !").build();
+        }
+        else if(id < 1 || id > articleManager.getAll().size() ||
+                article.getId() < 1 || article.getId() > articleManager.getAll().size()) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("ID inexistant !").build();
+        }
+        else if(!article.getId().equals(id)) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("ID mismatch !").build();
+        }
+        else {
+            if(!articleManager.update(article, id)) {
+                return Response.status(Response.Status.BAD_REQUEST)
+                        .entity("Article non existant !").build();
+            }
+            return Response.ok().entity(articleManager.getById(id)).build();
         }
     }
 }
---
+++ src/main/java/com/demo/api/business/ArticleManager.java
@@ -0,0 +1,49 @@
+package com.demo.api.business;
+
+import com.demo.api.data.Article;
+import com.demo.api.data.DAO;
+
+import java.util.List;
+
+public class ArticleManager {
+    private final DAO<Article> articleDAO = new DAO<>(Article.class);
+
+    private static ArticleManager articleManager;
+    public static ArticleManager getInstance() {
+        if(articleManager == null) {
+            articleManager = new ArticleManager();
+        }
+        return articleManager;
+    }
+
+    private ArticleManager() {
+    }
+
+    public List<Article> getAll(){
+        return articleDAO.findAll();
+    }
+
+    public void add(Article article) {
+        articleDAO.save(article);
+    }
+
+    public Article getById(Integer id) {
+        return articleDAO.findById(id);
+    }
+
+    public void delete(Article article){
+        articleDAO.delete(article);
+    }
+
+    public boolean update(Article articleNew, int id) {
+        Article articleExisting = articleDAO.findById(id);
+
+        if(articleExisting != null) {
+            articleDAO.update(articleNew);
+            return true;
+        }
+
+        return false;
+    }
+
+}
